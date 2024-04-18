ac42f6f0e1d35ef5686fa8396dd20fcbb35bd31e
hyunsb <junghss0113@gmail.com>
2024-04-18 23:53:36 +0900

    Refactor: region 검증 예외 클래스 세분화 및 메서드 명 변경

--- used-trading-application/src/main/java/org/flab/hyunsb/application/service/CategoryService.java
+++ used-trading-application/src/main/java/org/flab/hyunsb/application/service/CategoryService.java
@@ -1,6 +1,9 @@
 package org.flab.hyunsb.application.service;
 
+import static org.flab.hyunsb.application.exception.message.RegionErrorMessage.INVALID_REGION_ID;
+
 import lombok.RequiredArgsConstructor;
+import org.flab.hyunsb.application.exception.constraint.ConstraintException;
 import org.flab.hyunsb.application.output.CategoryOutputPort;
 import org.flab.hyunsb.application.validator.CategoryValidator;
 import org.springframework.stereotype.Service;
@@ -13,6 +16,7 @@ public class CategoryService implements CategoryValidator {
 
     @Override
     public void validateCategoryId(Long id) {
-        categoryOutputPort.findIdById(id).orElseThrow(IllegalArgumentException::new);
+        categoryOutputPort.findIdById(id).orElseThrow(() ->
+            new ConstraintException(INVALID_REGION_ID.getMessage()));
     }
 }
--- used-trading-application/src/main/java/org/flab/hyunsb/application/usecase/post/CreatePostUseCase.java
+++ used-trading-application/src/main/java/org/flab/hyunsb/application/usecase/post/CreatePostUseCase.java
@@ -4,5 +4,5 @@ import org.flab.hyunsb.domain.post.PostForCreate;
 
 public interface CreatePostUseCase {
 
-    Long createMember(PostForCreate postForCreate);
+    Long createPost(PostForCreate postForCreate);
 }
