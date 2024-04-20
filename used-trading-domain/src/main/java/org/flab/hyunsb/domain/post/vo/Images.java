39e6262c42c08da474b312e5f62f3e11ef00b279
hyunsb <junghss0113@gmail.com>
2024-04-20 23:06:35 +0900

    Refactor: images 도메인 생성 로직 변경 thumbnailNumber 유무에 따른 팩토리 메서드를 퍼블릭 인터페이스로 제공하는 방식에서 생성자를 통해 구분하는 방식으로 변경

--- used-trading-domain/src/main/java/org/flab/hyunsb/domain/post/vo/Images.java
+++ used-trading-domain/src/main/java/org/flab/hyunsb/domain/post/vo/Images.java
@@ -1,35 +1,36 @@
 package org.flab.hyunsb.domain.post.vo;
 
 import java.util.List;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.flab.hyunsb.domain.post.exception.PostConstraintException;
 
 @Getter
-@AllArgsConstructor
 public class Images {
+
+    private static final int DEFAULT_THUMBNAIL_NUMBER = 0;
 
     private String thumbnail;
     private List<String> images;
 
-    public static Images generateImagesExcludeThumbnail(List<String> images) {
-        validateImages(images);
-        return generateImagesIncludeThumbnail(0, images);
+    public Images(List<String> images) {
+        this(DEFAULT_THUMBNAIL_NUMBER, images);
     }
 
-    private static void validateImages(List<String> images) {
+    public Images(int thumbnailNumber, List<String> images) {
+        validateImages(images);
+        validateThumbnailNumber(thumbnailNumber, images.size());
+
+        thumbnail = images.get(thumbnailNumber);
+        this.images = images;
+    }
+
+    private void validateImages(List<String> images) {
         if (images.isEmpty()) {
             throw new PostConstraintException("이미지는 필수");
         }
     }
 
-    public static Images generateImagesIncludeThumbnail(int thumbnailNumber, List<String> images) {
-        validateImages(images);
-        validateThumbnailNumber(thumbnailNumber, images.size());
-        return new Images(images.get(thumbnailNumber), images);
-    }
-
-    private static void validateThumbnailNumber(int thumbnailNumber, int imageNumber) {
+    private void validateThumbnailNumber(int thumbnailNumber, int imageNumber) {
         if (thumbnailNumber >= imageNumber) {
             throw new PostConstraintException("썸네일 번호가 유효하지 않음");
         }
